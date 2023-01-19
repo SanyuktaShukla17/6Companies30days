0da3cc621d3598ff4901c079f8170e76908889df
SanyuktaShukla17 <114808389+SanyuktaShukla17@users.noreply.github.com>
2023-01-20 02:19:33 +0530

    Create NumberOfPairsSatisfyingInequality.java

---
+++ Microsoft/NumberOfPairsSatisfyingInequality.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long numberOfPairs(int[] nums1, int[] nums2, int diff) {
+        dif = diff;
+        for (int i = 0; i < nums1.length; i++) nums1[i] -= nums2[i];
+        return mergeSort(nums1, 0, nums1.length - 1);
+    }
+    private int dif;
+    //Time: O(lgN * N); Space:O(lgN)
+    private long mergeSort(int[] nums, int left, int right) {
+        if (right == left) return 0;
+
+        int mid = (left + right) / 2;
+        long count = mergeSort(nums, left, mid) + mergeSort(nums, mid + 1, right);
+        for (int i = mid, j = right; i >= left && j >= mid + 1;){
+            if (nums[i] <= nums[j] + dif) {
+                count += i - left + 1;j--;
+            } else i--;
+        }
+        merge(nums, left, mid, mid + 1, right);
+        return count;
+    }
+
+    //Time: O(N); Space:O(N)
+    private void merge(int[] nums, int left1, int right1, int left2, int right2){
+        if (left1 == left2) return;
+        int[] data = Arrays.copyOfRange(nums, left1, right1 + 1);
+        int idx = 0;
+        while (idx < data.length && left2 <= right2) {
+            nums[left1++] = data[idx] <= nums[left2] ? data[idx++] : nums[left2++];
+        }
+        while (idx < data.length) nums[left1++] = data[idx++];
+        while (left2 <= right2) nums[left1++] = nums[left2++];
+    }
+}
